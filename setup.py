d6f69ba41de1b83b552716bbb56ce5246c663072
Morteza Nourelahi Alamdari <me@mortezana.com>
2018-02-02 19:18:19 +0330

    Bug fixes on setup file.

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ import os
 from glob import glob
 from setuptools import setup, find_packages
 
+sites_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "sites")
 sites_ini = glob(os.path.join("sites", "*.ini"))
 
 setup(
     name="samplecmd",
-    version="1.0.4",
+    version="1.0.6",
     description="SampleCMD is a CLI application which prepares sample "
                 "commands for users based on search words.",
     author="Morteza Nourelahi Alamdari",
@@ -17,7 +18,7 @@ setup(
     url="https://github.com/mortezaipo/samplecmd/",
     packages=find_packages(),
     install_requires=['colorclass', 'lxml', 'requests'],
-    data_files=[("sites", sites_ini)],
+    data_files=[(sites_path, sites_ini)],
     include_package_data=True,
     python_requires='>=3',
     entry_points={
