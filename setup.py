c8b8c717daede1c1f71d43d240c0df1e13942ebd
Morteza Nourelahi Alamdari <me@mortezana.com>
2018-02-02 18:02:12 +0330

    Bug fixes on setup file.

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setup(
     version="1.0.0",
     description="SampleCMD is a CLI application which prepares sample "
                 "commands for users based on search words.",
-    author="Morteza Nourelahi Alamdari <me@mortezana.com>",
+    author="Morteza Nourelahi Alamdari",
+    author_email="me@mortezana.com",
     keywords=["samplecmd", "cmd", "cli", "search"],
     license="GPLv3.0",
     url="https://github.com/mortezaipo/samplecmd/",
