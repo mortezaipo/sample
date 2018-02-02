9de1ac3ffe56d0d6b118f457da3f454be9d17ff2
Morteza Nourelahi Alamdari <me@mortezana.com>
2018-02-02 21:29:53 +0330

    Update setup files.

--- setup.py
+++ setup.py
@@ -3,22 +3,21 @@ import os
 from glob import glob
 from setuptools import setup, find_packages
 
-sites_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "sites")
 sites_ini = glob(os.path.join("sites", "*.ini"))
 
 setup(
     name="samplecmd",
-    version="1.0.6",
+    version="1.0.7",
     description="SampleCMD is a CLI application which prepares sample "
                 "commands for users based on search words.",
     author="Morteza Nourelahi Alamdari",
     author_email="me@mortezana.com",
-    keywords=["samplecmd", "cmd", "cli", "search"],
+    keywords=["samplecmd", "cmd", "cli", "search", "command"],
     license="GPLv3.0",
     url="https://github.com/mortezaipo/samplecmd/",
     packages=find_packages(),
     install_requires=['colorclass', 'lxml', 'requests'],
-    data_files=[(sites_path, sites_ini)],
+    data_files=[("sites", sites_ini)],
     include_package_data=True,
     python_requires='>=3',
     entry_points={
