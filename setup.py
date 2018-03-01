4f07427f7d853ba07c549810732134fcd3b4ae36
Morteza Nourelahi Alamdari <me@mortezana.com>
2018-03-01 22:10:02 +0330

    Add python package classifiers.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="samplecmd",
-    version="1.1.5",
+    version="1.1.6",
     description="SampleCMD is a CLI application which prepares sample "
                 "commands for users based on search words.",
     long_description = open("README.rst").read(),
@@ -16,6 +16,23 @@ setup(
     install_requires=['colorclass', 'lxml', 'requests'],
     include_package_data=True,
     python_requires='>=3',
+    classifiers=[
+        "Development Status :: Production/Stable",
+        "Environment :: Console",
+        "Environment :: Linux",
+        "Environment :: MacOS X",
+        "Environment :: Win32 (MS Windows)",
+        "License :: OSI Approved :: GPL-v3",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Terminals",
+    ],
     entry_points={
         "console_scripts": [
             "samplecmd=samplecmd.samplecmd:main",
