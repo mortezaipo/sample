c8cd5bbb9ff24879fd068f4d6425801532c12177
Morteza Nourelahi Alamdari <me@mortezana.com>
2018-03-01 22:00:28 +0330

    Bug fixes.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version="1.1.5",
     description="SampleCMD is a CLI application which prepares sample "
                 "commands for users based on search words.",
-    long_description = open("README.rst").read()
+    long_description = open("README.rst").read(),
     author="Morteza Nourelahi Alamdari",
     author_email="me@mortezana.com",
     keywords=["samplecmd", "cmd", "cli", "search", "command"],
