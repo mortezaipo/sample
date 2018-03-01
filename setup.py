8e18a6c0ed33897bdd26e4bfd8b0ead84c4b9b9a
Morteza Nourelahi Alamdari <me@mortezana.com>
2018-03-01 21:59:41 +0330

    Add long-description to the python setup file.

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="samplecmd",
-    version="1.1.4",
+    version="1.1.5",
     description="SampleCMD is a CLI application which prepares sample "
                 "commands for users based on search words.",
+    long_description = open("README.rst").read()
     author="Morteza Nourelahi Alamdari",
     author_email="me@mortezana.com",
     keywords=["samplecmd", "cmd", "cli", "search", "command"],
